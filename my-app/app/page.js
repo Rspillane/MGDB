13a43235f968049421fc20dc2582b8ba6ef0aa36
Robert Spillane <robert.e.spillane@gmail.com>
2023-07-02 22:43:10 +0100

    Button to add new game card/open form

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -4,11 +4,17 @@ import GameCard from './components/gameCard'
 
 export default function Home() {
 
+  //I don't know how to add stuff interactively in React
+  const cardContainer = .getElementsByClassName("cards-container")[0]
 
+  const createNewGame = () => {
+    cardContainer.append(<GameCard></GameCard>);
+  }
   return (
     <main className={styles.main}>
       <h1>My Game Vault</h1>
       <div className='cards-container'>
+        <button onClick={createNewGame}>New Game</button>
         <GameCard title="Metroid" console="GameCube" rating="10"/>
         <GameCard title="Mario" console="N64" rating="2"/>
       </div>
