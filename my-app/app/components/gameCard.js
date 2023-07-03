d844ed9956846715b8ba7588b873b47f339af534
Robert Spillane <robert.e.spillane@gmail.com>
2023-07-03 23:44:49 +0100

    Form created to take user gamecard inputs

--- my-app/app/components/gameCard.js
+++ my-app/app/components/gameCard.js
@@ -4,15 +4,13 @@ import "./gameCard.css";
 
 export default function GameCard({title, console, rating}) {
 
-    const [expand, setExpand] = useState(false);
-
     const [isShown, setIsShown] = useState(false);
 
     return (
-        <div className="card-container"
-        onMouseEnter={() => setIsShown(true)}
-        onMouseLeave={() => setIsShown(false)}>
-            <h1 className="game-title"> {title}</h1>
+      <div className="card-container"
+      onMouseEnter={() => setIsShown(true)}
+      onMouseLeave={() => setIsShown(false)}>
+        <h1 className="game-title"> {title}</h1>
         {isShown && (
         <div className="card-content">
             <p> Platform:     {console}</p>
--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,20 +1,22 @@
 import styles from './page.module.css'
 import GameCard from './components/gameCard'
 import games from './objects'
+import MyForm from './components/form';
 
 export default function Home() {
 
   const listItems = games.map(game => 
-  <GameCard title={game.title} console={game.console} rating={game.rating}/>
+    <GameCard title={game.title} console={game.console} rating={game.rating}/>
   );
 
   return (
     <main className={styles.main}>
+      <MyForm></MyForm>
       <h1>My Game Vault</h1>
       <div className='cards-container'>
         <ul>{listItems}</ul>
         <button>New Game</button>
       </div>
     </main>
-  )
+  );
 }
