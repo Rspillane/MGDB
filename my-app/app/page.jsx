8f77e9197762658b8bd4d1903dbdf13bec79942d
Robert Spillane <robert.e.spillane@gmail.com>
2023-08-10 22:38:12 +0100

    user search value related to displayed games, state added for userinput and matched results

--- my-app/app/page.jsx
+++ my-app/app/page.jsx
@@ -7,12 +7,14 @@ import './sections/vault/vault.css';
 import GameCard from './components/gameCard';
 
 
-import SearchVar from './sections/header/searchVar';
+// import SearchVar from './sections/header/searchVar';
 
 
 export default function Home() {
-  const [searchResults, setSearchResults] = useState([]);
+  const [searchResults, setSearchResults] = useState([])
   const [isLoading, setLoading] = useState(false)
+  const [result, setResult] = useState([])
+  const [userInput, setUserInput] = useState('')
 
   useEffect(() => {
     // Update the document title using the browser API
@@ -28,12 +30,29 @@ export default function Home() {
   if (isLoading) return <p>Loading...</p>
   if (!searchResults) return <p>No profile data</p>
 
-  //if i want the vault to update on search, 
-  //then searchResults should relate to the search bar
-  //not the response of the api request
-  const gameList = searchResults.map((prop) => prop.name)
+  // if userInput is == '' the display gameList
+    const gameList = searchResults.map((prop) => prop.name)
 
-  const listItems = searchResults.map(prop => 
+  // const listItems = searchResults.map(prop => 
+  //   <GameCard 
+  //     title={prop.name} 
+  //     image_id={prop.cover.image_id} 
+  //     summary={prop.summary} 
+  //     genres={prop.genres.map((genre) => genre.name + " ")}
+  //     platforms={prop.platforms.map((platform) => platform.name + " ")}
+  //   />
+  // );
+  
+  function getUserInput() {
+    setUserInput(document.getElementById('search-box').value)
+
+    setResult('')
+    setResult(() => searchResults.filter((searchResults) => searchResults.name.toLowerCase().includes(userInput)))
+    console.log(result); 
+  }
+
+
+  const listItems = result.map(prop => 
     <GameCard 
       title={prop.name} 
       image_id={prop.cover.image_id} 
@@ -42,22 +61,22 @@ export default function Home() {
       platforms={prop.platforms.map((platform) => platform.name + " ")}
     />
   );
-
   
+
 
   {/* Have state for games in this file and pass in save method*/}
   return (
-    <main className={styles.main}>
+    <main className={ styles.main }>
       <section id='search-section'>
         <h1> Search Section </h1>
         <div className='search'>
-          <SearchVar games={gameList}></SearchVar>
-          {/* i want to use search as a generic component for both titles and genres with the cached responses as the list input */}
+          <div class="search-text">Search:</div>
+          <input id="search-box" onKeyUp={ getUserInput }/>
         </div>
       </section>
       <h1>Game Vault</h1>
       <div className='cards-container'>
-        {listItems}
+          { listItems }
       </div>
     </main>
   );
