6b6cff8e632de8584e82a945adb3d7d7b2352117
Robert Spillane <robert.e.spillane@gmail.com>
2024-04-04 10:53:16 +0100

    production stuck on loading page, output:export removed for next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  runtime: 'edge',
+  // output: 'export',
+  // runtime: 'edge',
 }
 
 module.exports = nextConfig
